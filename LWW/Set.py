563b438cd7105a3b56d623f76c23b89fe4eeb560
Rauan Akylzhanov <rauan.akylzhanov@kcell.kz>
2021-05-06 13:16:53 +0600

    LWW reorganized

--- LWW/Set.py
+++ LWW/Set.py
@@ -1,6 +1,10 @@
 import time
 from threading import RLock
-class LWWSet:
+
+from time import time
+
+
+class Set:
 	'''
 		A Last-Write-Wins Set CRDT implementation
 	'''
@@ -11,14 +15,21 @@ class LWWSet:
 		self.lock_when_adding = RLock()
 		self.lock_when_removing = RLock()
 	
-	def add(self, element, timestamp):
-		added_elements = self.added
+	def add(self, element):
+		# we create a timestamp at the time of adding element
+		timestamp = time()
+
+		added = self.added
 		if element in added:
+			 # if element is in added, then 
+			# we do not update timestamp
 			current_timestamp = added[element]
+		
 			if current_timestamp < timestamp:
-                added[element] = timestamp
-        else:
-            added[element] = timestamp
-	
+				# adding same element later 
+				added[element] = timestamp
+		else:
+			added[element] = timestamp
+
 		
 		
